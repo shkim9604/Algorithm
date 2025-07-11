99c4b2f20eb54003b271b09304bba05a121c7ef3
shkim9604@naver.com <shkim9604@naver.com>
2025-07-11 23:25:44 +0900

    lv1,문자열다루기기본,Time:0.06ms,Memory:9.25MB

---
+++ 프로그래머스/LV1/문자열 다루기 기본/문자열 다루기 기본.py
@@ -0,0 +1,11 @@
+def solution(s):
+    answer = True
+    if len(s) == 4 or len(s) == 6:
+        if s.isdigit():
+            pass
+        else:
+            return False
+    else:
+        return False
+
+    return answer
